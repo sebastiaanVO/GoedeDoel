5985c6706f54462496c0b25854dc9aef98736c68
SebastiaanVO <s.van.overschee@gmail.com>
2024-12-13 11:37:28 +0100

    Javadoc proberen

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -2,7 +2,6 @@ package be.vdab.goededoel;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import be.vdab.goededoel.GoedeDoel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
